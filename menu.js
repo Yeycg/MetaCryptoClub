404457607654f9f50567441a37fe200407647959
Yey Castano <yeygonzalez@gmail.com>
2022-09-20 22:10:00 -0400

    Iniciamos git para probar diferentes formas de maquetar un video

---
+++ menu.js
@@ -0,0 +1,8 @@
+const btnMenuMobile = document.querySelector('.btn-mobile-menu');
+const menuMobile = document.querySelector('.menuMobile');
+
+btnMenuMobile.addEventListener('click', toggleMobilemenu) ;
+
+function toggleMobilemenu() {
+    menuMobile.classList.toggle ('inactive');
+}
