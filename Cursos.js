e35a6422fd1a387795df62b7258cfeb1367062da
Yey Castano <yeygonzalez@gmai.com>
2023-03-25 20:15:54 -0400

    cambios para toda la web 7

--- Cursos.js
+++ Cursos.js
@@ -1,19 +1,4 @@
-//Seccion contenido del curso 
 
-const toggleButtons = document.querySelectorAll('.card-toggle');
-
-toggleButtons.forEach((button) => {
-  button.addEventListener('click', () => {
-    const cardBody = button.parentNode.parentNode.nextElementSibling;
-    if (cardBody.classList.contains('hidden')) {
-      cardBody.classList.remove('hidden');
-      button.innerHTML = 'Ver menos';
-    } else {
-      cardBody.classList.add('hidden');
-      button.innerHTML = 'Ver más';
-    }
-  });
-});
 
 
 
